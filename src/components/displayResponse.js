94b9eef2bc862ed8f23b25dc95bdbab7dc50e1ad
Aditya Kumar Sharma <adityakd171@gmail.com>
2023-06-03 22:43:30 +0530

    json response display added

--- src/components/displayResponse.js
+++ src/components/displayResponse.js
@@ -1,14 +1,24 @@
 import React, {useState} from 'react'
-import jsonData from '../response/sample_response_1.json'
+// import jsonData from '../response/sample_response_1.json'
 
 
 function DisplayResponse() {
     const [jsonObject, setJsonObject ] = useState(null);
 
-    const handleButtonClick = () => {
+    const handleButtonClick = async () => {
 
-        const jsonString = JSON.stringify(jsonData);
-        setJsonObject(jsonString);
+        /* Static Response */
+        // const jsonString = JSON.stringify(jsonData);
+        // setJsonObject(jsonString);
+
+        try{
+            const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
+            const data = await response.json();
+            const jsonString = JSON.stringify(data);
+            setJsonObject(jsonString);
+        }catch(error){
+            console.error('Error fetching JSON:', error);
+        }
     };
     return (
         <div>
